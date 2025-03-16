9bcc02248d74c875fdeaf285b7278e6a40c0b7bf
lobov <lobov.vitaliy@gmail.com>
2025-03-16 14:53:09 +0300

    Initial Commit

---
+++ TravelSchedule/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  TravelSchedule
+//
+//  Created by Vitaly Lobov on 16.03.2025.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ TravelSchedule/TravelScheduleApp.swift
@@ -0,0 +1,17 @@
+//
+//  TravelScheduleApp.swift
+//  TravelSchedule
+//
+//  Created by Vitaly Lobov on 16.03.2025.
+//
+
+import SwiftUI
+
+@main
+struct TravelScheduleApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
